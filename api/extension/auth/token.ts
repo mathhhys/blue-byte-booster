5624b57351cff28325d0e336fbe502fe1aeeaf49
mathhhys <mathys@softcodes.io>
2025-09-24 17:04:06 +0200

    Revert

--- api/extension/auth/token.ts
+++ api/extension/auth/token.ts
@@ -1,6 +1,6 @@
 import { verifyToken } from '@clerk/backend';
 import { createClient } from '@supabase/supabase-js';
-import { generateSessionId, generateJWT, verifyJWT } from '../../utils/jwt.js';
+import { generateSessionId, generateJWT, verifyJWT } from '../../../utils/jwt';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
