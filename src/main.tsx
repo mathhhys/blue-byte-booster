ab6deb2a2cb4398b56a68497fb0e33fbfd281949
mathhhys <mathys@softcodes.io>
2025-11-20 14:13:33 +0100

    Hotjar tracking

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { AuthErrorBoundary } from './components/auth/AuthErrorBoundary'
 import { Analytics } from '@vercel/analytics/react'
+import Hotjar from '@hotjar/browser';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -11,6 +12,10 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
+const siteId = 6581430;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 createRoot(document.getElementById("root")!).render(
   <AuthErrorBoundary>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
